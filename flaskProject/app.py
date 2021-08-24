10ecd3b9d437a0d0e8c2e3122c35a40f64ae0ec6
theinfamousmrmeow <alexander.d.macey@gmail.com>
2021-08-24 13:49:14 -0400

    AJAX stuff

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# configuration
+DEBUG = True
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'GO TO BED!'
+    return 'DONT GO TO BED!'
+
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong from App.py!')
 
 
 if __name__ == '__main__':
